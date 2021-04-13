8d7c4fb83881397f288af00a1aea3ee7d88fcd32
Jeffrey J. Bryan <DracoNoir@users.noreply.github.com>
2021-04-13 15:17:48 -0700

    Implements Actor interface, with TODO for acting.

--- src/commonMain/kotlin/com/nocteq/petridish/Substance.kt
+++ src/commonMain/kotlin/com/nocteq/petridish/Substance.kt
@@ -9,23 +9,29 @@ package com.nocteq.petridish
  * equal to or less than [dissolution], then the substance dissolves into nothing; if the resulting amount exceeds
  * [saturation], then the excess amount evaporates into nothing.
  */
-sealed class Substance(open var intensity: Float, open val dissolution: Float, open val saturation: Float) : Actor
+sealed class Substance(open var intensity: Float, open val dissolution: Float, open val saturation: Float) : Actor {
+    override fun act(petriDish: PetriDish) {
+        // TODO diffuse substance
+    }
+}
 
 /** An energy. */
 data class Energy(
     val type: Type,
     override var intensity: Float = 0f,
 ) : Substance(intensity, type.dissolution, type.saturation) {
-    enum class Type(val dissolution: Float = 0f, val saturation: Float = 1f) {
-        ELECTRICITY,
-        HEAT,
-        LIGHT,
-        MAGNETISM,
-        RADIATION_ALPHA,
-        RADIATION_BETA,
-        RADIATION_GAMMA,
-        ULTRAVIOLET,
+    enum class Type(val codePrefix: String, val dissolution: Float = 0f, val saturation: Float = 1f) {
+        ELECTRICITY("E"),
+        HEAT("H"),
+        LIGHT("L"),
+        MAGNETISM("M"),
+        RADIATION_ALPHA("RA"),
+        RADIATION_BETA("RB"),
+        RADIATION_GAMMA("RG"),
+        ULTRAVIOLET("U"),
     }
+
+    override val code: String get() = "${type.codePrefix}@$intensity"
 }
 
 /** A physical substance. */
@@ -33,16 +39,17 @@ data class Agent(
     val type: Type,
     override var intensity: Float = 0f,
 ) : Substance(intensity, type.dissolution, type.saturation) {
-    enum class Type(val dissolution: Float = 0f, val saturation: Float = 1f) {
-        ACID,
-        CARBOHYDRATE,
-        DARKENER, // resists LIGHT and ULTRAVIOLET
-        HYDROCARBON,
-        INSULATOR, // resists HEAT
-        NEUTRAL,
-        PRESSURE,
-        RESISTOR, // resists ELECTRICITY and MAGNETISM
-        SHIELDING, // resists RADIATION_*
-        THICKENER, // resists movement
+    enum class Type(val codePrefix: String, val dissolution: Float = 0f, val saturation: Float = 1f) {
+        ACID("A"),
+        CARBOHYDRATE("CH"),
+        DARKENER("-LU"), // resists LIGHT and ULTRAVIOLET
+        HYDROCARBON("HC"),
+        INSULATOR("-H"), // resists HEAT
+        PRESSURE("P"),
+        RESISTOR("-EM"), // resists ELECTRICITY and MAGNETISM
+        SHIELDING("-R*"), // resists RADIATION_*
+        THICKENER("T"), // resists movement
     }
+
+    override val code: String get() = "${type.codePrefix}@$intensity"
 }
